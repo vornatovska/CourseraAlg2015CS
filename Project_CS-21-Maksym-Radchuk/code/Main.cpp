2750b3a841673455ef281e43b267f0d82bd588d7
Max <maxr96@gmail.com>
2015-05-19 02:05:18 +0300

    Init

--- Project_CS-21-Maksym-Radchuk/code/Main.cpp
+++ Project_CS-21-Maksym-Radchuk/code/Main.cpp
@@ -2,17 +2,25 @@
 #include <iomanip>
 #include <stdlib.h>
 #include <Windows.h>
-#include "Jordan.h"
+#include "Jordan.cpp"
 
 using namespace std;
 
 
 
-int main(){
-	SetConsoleCP(1251);
+int main()
+{ 
+    SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-	Jordan jo;
-	jo.setJordan();
-	jo.doCalculate();
-	
+    int a;
+    Jordan jo;
+    cout<<"1 - Для вводу матриці з файлу"<<endl<<"2 - Для вводу матриці з консолі"<<endl;
+    cin>>a;
+    if(a==1){
+            jo.fromFile();}
+    else if(a==2){
+         jo.setJordan();}  
+    jo.doCalculate();
+    system("pause");
+    return 0;
 }
